899a19e72dac8e6aad446ba8c8ba26f054922a9b
moeezs82 <mora03033@gmail.com>
2023-04-06 19:46:01 +0500

    fixed error

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,42 +1,43 @@
-import getUser from "@/lib/getUser";
-import getUserPosts from "@/lib/getUserPosts";
-import { Suspense } from "react";
-import UserPosts from "./components/UserPosts";
+import getUser from "@/lib/getUser"
+import getUserPosts from "@/lib/getUserPosts"
+import { Suspense } from "react"
+import UserPosts from "./components/UserPosts"
 import type { Metadata } from 'next'
 
 type Params = {
-  params: {
-    id: string;
-  };
-};
+    params: {
+        id: string
+    }
+}
 
 export async function generateMetadata({ params: { id } }: Params): Promise<Metadata> {
-  const userData: Promise<User> = getUser(id)
-  const user: User = await userData
+    const userData: Promise<User> = getUser(id)
+    const user: User = await userData
 
-  return {
-      title: user.name,
-      description: `This is the page of ${user.name}`
-  }
+    return {
+        title: user.name,
+        description: `This is the page of ${user.name}`
+    }
 
 }
 
-export default async function User({ params: { id } }: Params) {
-  // requesting 2 api calls in parallel for better performance as recommended by next js
-  const userData: Promise<User> = getUser(id);
-  const userPostsData: Promise<Post[]> = getUserPosts(id);
+export default async function UserPage({ params: { id } }: Params) {
+    const userData: Promise<User> = getUser(id)
+    const userPostsData: Promise<Post[]> = getUserPosts(id)
 
-  //   const [user, userPosts] = await Promise.all([userData, userPostsData]);
-  const user = await userData;
+    // If not progressively rendering with Suspense, use Promise.all
+    //const [user, userPosts] = await Promise.all([userData, userPostsData])
 
-  return (
-    <>
-      <h2>{user.name}</h2>
-      <br />
-      <Suspense fallback={<h2>Loading...</h2>}>
-        {/* @ts-expect-error Async Server Component */}
-        <UserPosts posts={userPostsData} />
-      </Suspense>
-    </>
-  );
+    const user = await userData
+
+    return (
+        <>
+            <h2>{user.name}</h2>
+            <br />
+            <Suspense fallback={<h2>Loading...</h2>}>
+                {/* @ts-expect-error Server Component */}
+                <UserPosts promise={userPostsData} />
+            </Suspense>
+        </>
+    )
 }
