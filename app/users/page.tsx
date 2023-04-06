c71f92e0382188e66ed636333b0c38aea9b2b65b
moeezs82 <mora03033@gmail.com>
2023-04-06 19:44:25 +0500

    fatched data

---
+++ app/users/page.tsx
@@ -0,0 +1,38 @@
+import getAllUsers from "@/lib/getAllUsers"
+import type { Metadata } from "next"
+import Link from "next/link"
+
+export const metadata: Metadata = {
+    title: "Users",
+    description: "Users page here"
+}
+
+export default async function Users() {
+
+    const usersData: Promise<User[]> = getAllUsers()
+
+    const users = await usersData
+
+    console.log("hello")
+
+    const content = (
+        <section>
+            <h2>
+                <Link href='/'>Back to Home</Link>
+            </h2>
+            <br />
+            {users.map(user => {
+                return (
+                    <div key={user.id}>
+                        <p>
+                            <Link href={`/users/${user.id}`}>{user.name}</Link>
+                        </p>
+                        <br />
+                    </div>
+                )
+            })}
+        </section>
+    )
+
+  return content
+}
